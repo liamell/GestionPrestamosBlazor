b30238ccbe8910aade64eceb9b1e6dc29ebd3dbb
Braylin <braylinvasquez20@gmail.com>
2024-10-12 20:02:59 -0400

    Agregando decorador Required y Range para validar los campos.

--- Models/Prestamos.cs
+++ Models/Prestamos.cs
@@ -11,14 +11,17 @@ public partial class Prestamos
     [Key]
     public int PrestamosId { get; set; }
 
+    [Required(ErrorMessage = "Este campo es requerido")]
     public string Concepto { get; set; } = null!;
 
+    [Range(1, double.MaxValue, ErrorMessage = "El monto no puede ser menor a 1")]
     public double Monto { get; set; }
 
     public double Balance { get; set; }
 
+    [Range(1, int.MaxValue, ErrorMessage = "Debe seleccionar un deudor válido")]
     public int DeudorId { get; set; }
-
+    
     [ForeignKey("DeudorId")]
     [InverseProperty("Prestamos")]
     public virtual Deudores Deudor { get; set; } = null!;
