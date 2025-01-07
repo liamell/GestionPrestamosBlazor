f1ce1e02c567705f592b875abe5931da178b3464
Enel Almonte <enel_almonte@ucne.edu.do>
2025-01-06 22:33:38 -0400

    Migrate to .net 9

--- GestionPrestamos.Blazor/Program.cs
+++ GestionPrestamos.Blazor/Program.cs
@@ -34,9 +34,10 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseStaticFiles();
+
 app.UseAntiforgery();
 
+app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
