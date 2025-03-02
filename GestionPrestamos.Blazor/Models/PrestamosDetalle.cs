08b1af3038a0b96dffdb494a2cdc53d45f032f37
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2025-03-01 22:33:32 -0500

    Modelo PrestamoDetalle

---
+++ GestionPrestamos.Blazor/Models/PrestamosDetalle.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GestionPrestamos.Models;
+
+public class PrestamosDetalle
+{
+    [Key]
+    public int DetalleId { get; set; }
+
+    [Required(ErrorMessage = "Campo Obligatorio")]
+    [ForeignKey("Prestamos")]
+    public int PrestamoId { get; set; }
+    public Prestamos Prestamos { get; set; }
+
+    public int CuotaNo { get; set; }
+    public DateTime Fecha { get; set; }
+    public decimal Valor { get; set; }
+    public decimal Balance { get; set; }
+
+
+
+}
